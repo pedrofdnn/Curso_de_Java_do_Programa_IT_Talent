3b639ba3af1c75de260b9a0fd94bd4629c19b605
Pedro Fernandes <pedrofdnn@gmail.com>
2024-02-07 17:14:48 -0300

    Curso de Java da Programa IT Talent

---
+++ src/Comentario.java
@@ -0,0 +1,21 @@
+public class Comentario {
+    public static void main(String[] args) {
+
+//         linha comentada aqui
+
+        /* Comentar uma linha: uso de Atalhos:
+
+        comentando somente uma linha:
+        Use Ctrl + / para comentar ou descomentar uma linha específica.
+        Certifique - se de usar o / localizado no teclado numérico.
+        Por exemplo, se você deseja comentar a linha atual, basta posicionar o cursor nela e pressionar Ctrl + /.
+         */
+
+  /*    Comentar um bloco de código:
+
+        Selecione o bloco de código que deseja comentar.
+        Use Ctrl + Shift + / para comentar ou descomentar o bloco selecionado.*/
+
+
+    }
+}
